634504ae9134ca2905cbb479c66120dc90f79584
hodeiez <hodeinaiz@hotmail.com>
2021-03-05 16:05:46 +0100

    playing around with controls

--- game.js
+++ game.js
@@ -1,28 +1,36 @@
 
 
-function detectKey(e,player) {
+function detectKey(e) {
 
-    var posLeft = document.getElementById(player).offsetLeft;
-    var posTop = document.getElementById(player).offsetTop;
+
+    let posLeft = document.getElementById("player").offsetLeft;
+    let posTop = document.getElementById("player").offsetTop;
 
     e = e || window.event;
 
-    if (e.keyCode == '38') {
+    if (e.keyCode == '32') {
         // up arrow
-        document.getElementById(player).style.marginTop  = (posTop-58)+"px";
+        document.getElementById("player").style.marginTop  = (posTop-58*4)+"px";
     }
-    
+
     else if (e.keyCode == '40') {
         // down arrow
-        document.getElementById(player).style.marginTop  = (posTop+58)+"px";
+        document.getElementById("player").style.marginTop  = (posTop+58)+"px";
     }
     else if (e.keyCode == '37') {
        // left arrow
-        document.getElementById(player).style.marginLeft  = (posLeft-58)+"px";
+        document.getElementById("player").style.marginLeft  = (posLeft-58)+"px";
+       
     }
     else if (e.keyCode == '39') {
        // right arrow
-        document.getElementById(player).style.marginLeft  = (posLeft+58)+"px";
+        document.getElementById("player").style.marginLeft  = (posLeft+58)+"px";
     }
+   if(posTop<document.getElementById("screen").offsetTop){
+        document.getElementById("player").style.backgroundColor ="red";
+        document.getElementById("player").style.marginTop= posTop+"px";
+        console.log(posTop);
+    }
+    
 
 }
